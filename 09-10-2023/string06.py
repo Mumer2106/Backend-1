240aeabdf30dba788f1e92c76b0d8c255b317011
Mumer2106 <70133894@student.uol.edu.pk>
2023-10-09 13:06:59 +0300

    six

---
+++ 09-10-2023/string06.py
@@ -0,0 +1,5 @@
+# Name= input("Enter your name: ")
+# print("Hey " + Name)
+Name= "emma"
+number= len(Name) * 3
+print("Hello {number}, your lucky number is {Name}." .format(Name="Name", number="number"))
