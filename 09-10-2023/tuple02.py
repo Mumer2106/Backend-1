9f791a43be98403c0e8ee2229e2b9d073a0d9ffa
Mumer2106 <70133894@student.uol.edu.pk>
2023-10-09 14:48:40 +0300

    tuple02

---
+++ 09-10-2023/tuple02.py
@@ -0,0 +1,8 @@
+def convert_seconds(sec):
+    hours= sec // 3600
+    minutes= (sec - hours * 3600)// 60
+    remaining_seconds= sec - hours * 3600 - minutes * 60
+    return hours, minutes, sec
+
+hours, minutes, sec = convert_seconds(5000)
+print(hours, minutes, sec)
